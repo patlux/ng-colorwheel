03e51e9f6c0e7cf0a32c6ba32b818153648ec628
Patrick Wozniak <patrick.wozniak@hiq5.com>
2015-05-24 00:55:02 +0200

    setting up gh-pages

---
+++ example/app.js
@@ -0,0 +1,57 @@
+// -------------------------------------------------------------------------- \\
+// EXAMPLE                                                                    \\
+// -------------------------------------------------------------------------- \\
+
+
+
+// APP CONTROLLER
+
+appController = function() {
+  vm = this;
+
+  // ----------------------------- \\
+
+  init = function() {
+
+    vm.color = '#'+(Math.random()*0xFFFFFF<<0).toString(16);
+    vm.colorChanged = colorChanged;
+
+    vm.colorHistory = []
+
+  }
+
+
+
+  colorChanged = function() {
+    console.log("color changed:", vm.color );
+    vm.colorHistory.push( vm.color );
+
+    if (vm.colorHistory.length > 50) {
+      vm.colorHistory.shift()
+    }
+  }
+
+
+
+  // ----------------------------- \\
+  init();
+  return;
+}
+
+
+
+// REVERSE LIST FILTER
+
+reverseFilter = function() {
+  return function(items) {
+    return items.slice().reverse()
+  }
+}
+
+// -------------------------------------------------------------------------- \\
+// ANGULAR MODULE
+
+angular
+  .module( 'ngColorwheelExample', [ 'directive.ngColorwheel' ] )
+  .controller('AppController', appController)
+  .filter('reverse', reverseFilter);
